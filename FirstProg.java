7ca6da83e7fee2a268b59f0b60ae0b66494912a2
Dino Nguyen <dinodnguyen@gmail.com>
2019-02-12 22:21:22 -0800

    Add FirstProg file

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Dino Nguyen, am determined to learn how to code." );
+    System.out.println( "Today's date is 02/12/2019." );
+  }
+}
