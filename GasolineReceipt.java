5593348bf7e2f6bbef6fa76d0e18dce9c85f5a06
Dino Nguyen <dinodnguyen@gmail.com>
2019-02-12 22:26:35 -0800

    Add gasoline receipt print file

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String [] args) {
+    System.out.println( "+____________________________+");
+    System.out.println( "|                            |");
+    System.out.println( "|       CORNER STORE         |");
+    System.out.println( "|                            |");
+    System.out.println( "|   2019-02-07    4:56 PM    |");
+    System.out.println( "|                            |");
+    System.out.println( "|   GALLONS:       7.64      |");
+    System.out.println( "|   PRICE/GALLON:  $2.07     |");
+    System.out.println( "|                            |");
+    System.out.println( "|   FUEL TOTAL:    $15.815   |");
+    System.out.println( "|                            |");
+    System.out.println( "+____________________________+");
+  }
+}
